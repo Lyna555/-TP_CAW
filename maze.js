21da49b88dac4380e2a02593dba49d635c907812
lyna555 <bouglina3@gmail.com>
2022-10-09 20:31:29 +0100

    modified:   maze.js

--- maze.js
+++ maze.js
@@ -3,14 +3,16 @@ var debut = document.getElementById("start");
 var fin = document.getElementById("end");
 var status = document.getElementById("status");
 var refair = document.getElementById("restart");
+var maze = document.getElementById("maze");
 var i = 0;
+var co = 1;
 
 debut.addEventListener("mouseover", function () {
   debut.style.background = "#eeeeee";
   document.getElementById("status").innerHTML = "Glissez la souris de S vers E sans toucher les bordures";
-
   while (i < boundaries.length) {
     boundaries[i].addEventListener("mouseover", function () {
+      co = 0;
       for (var j = 0; j < boundaries.length; j++) {
         boundaries[j].style.background = "red";
       }
@@ -22,12 +24,15 @@ debut.addEventListener("mouseover", function () {
 });
 
 fin.addEventListener("mouseover", function () {
-  document.getElementById("status").innerHTML = "Vous avez gagne!";
-  fin.style.background = "#eeeeee";
-  for (var i = 0; i < boundaries.length; i++) {
-    boundaries[i].style.background = "#88ff88";
+  if (co == 1) {
+    document.getElementById("status").innerHTML = "Vous avez gagne!";
+    fin.style.background = "#eeeeee";
+    for (var i = 0; i < boundaries.length; i++) {
+      boundaries[i].style.background = "#88ff88";
+    }
+    alert("Congratulation!");
   }
-  alert("Congratulation!")
+  co = 1;
 });
 
 
@@ -36,6 +41,8 @@ refair.addEventListener("click", function () {
   for (var i = 0; i < boundaries.length; i++) {
     boundaries[i].style.background = "#eeeeee";
   }
+  debut.style.background = "#88ff88";
+  fin.style.background = "#8888ff";
 }
 )
 
