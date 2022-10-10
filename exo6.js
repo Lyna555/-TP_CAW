b8d82234d06ebb02af3e8435b75ae8081fd2405d
lyna555 <bouglina3@gmail.com>
2022-10-10 20:08:54 +0100

    new file:   exo1.js 	new file:   exo2.js 	new file:   exo3.js 	new file:   exo4.js 	new file:   exo6.js 	new file:   exo7.js 	deleted:    maze.js

---
+++ exo6.js
@@ -0,0 +1,62 @@
+let boundaries = document.getElementsByClassName("boundary");
+let debut = document.getElementById("start");
+let fin = document.getElementById("end");
+let status = document.getElementById("status");
+let maze = document.getElementById("maze");
+let i = 0;
+let win = true;
+status.innerHTML = "Bonjour!";
+
+
+debut.addEventListener("click", function () {
+  status.innerHTML = 'Bonjour!';
+  debut.style.background = "#88ff88";
+  fin.style.background = "#8888ff";
+  for (var i = 0; i < boundaries.length; i++) {
+    boundaries[i].style.background = "#eeeeee";
+  }
+});
+
+debut.addEventListener("mouseover", function () {
+ if(status.innerHTML != "Vous avez Perdue!" && status.innerHTML != "Vous avez gagne!"){
+  status.innerHTML = 'Glissez la souris de "S" vers "E" sans toucher les bordures';
+  debut.style.background = "#eeeeee"
+  while (i < boundaries.length) {
+    boundaries[i].addEventListener("mouseover", function () {
+      if (status.innerHTML == 'Glissez la souris de "S" vers "E" sans toucher les bordures') {
+        win = false;
+        for (var j = 0; j < boundaries.length; j++) {
+          boundaries[j].style.background = "red";
+        }
+        status.innerHTML = "Vous avez Perdue!";
+        stopPropagation();
+      }
+    });
+    i++;
+  }
+  maze.addEventListener("mouseleave", function () {
+    if (status.innerHTML == 'Glissez la souris de "S" vers "E" sans toucher les bordures') {
+      alert("Vous etes hors bordure!")
+      status.innerHTML = 'Bonjour!';
+      debut.style.background = "#88ff88";
+      fin.style.background = "#8888ff";
+      for (var i = 0; i < boundaries.length; i++) {
+        boundaries[i].style.background = "#eeeeee";
+      }
+    }
+  })
+ }
+}
+)
+
+fin.addEventListener("mouseover", function () {
+
+  if (win == true && status.innerHTML != "Vous avez Perdue!" && status.innerHTML != "Bonjour!") {
+    fin.style.background = "#eeeeee"
+    status.innerHTML = "Vous avez gagne!";
+    for (var i = 0; i < boundaries.length; i++) {
+      boundaries[i].style.background = "green";
+    }
+  }
+  win = true
+});
