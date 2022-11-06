136adc794bcd74e9ffa75a5018c51fb830510e19
lyna555 <bouglina3@gmail.com>
2022-11-06 23:06:01 +0100

    modified:   TP5 (Node)/exo3-2.js

--- TP5 (Node)/exo3-2.js
+++ TP5 (Node)/exo3-2.js
@@ -4,80 +4,130 @@ var arg = process.argv
 var args = arg.slice(3)
 
 function node(word) {
-    const reg = new RegExp(word,'i')
+    const reg = new RegExp(word, 'i')
     let co = 0
     let d = 0
     let t = []
     let tab = []
     let e = []
     let cp
+    let tb = []
+    let c = 0
 
-    if (arg[3].match('.txt')) {
-        args.forEach(file => {
-            fs.readFile(file, function (err, data) {
+    for (let i = 0; i <= args.length - 1; i++) {
+        if (!args[i].match('.txt')) {
+            c++
+        }
+    }
+
+    if (c == args.length) {
+        args.forEach(folder => {
+            fs.readdir(folder, function (err, files) {
                 if (err) {
-                    console.log("Le fichier '" + file + "' n'existe pas.")
+                    console.log("Le dossier '" + args[0] + "' n'existe pas.")
                 } else {
-                    if (reg.test(data)) {
-                        console.log('Found in: ' + file)
-                    } else {
-                        d++
-                    }
-                    if (d == args.length) {
-                        console.log("NONE")
-                    }
-                }
-            });
-        });
-    } else {
-        fs.readdir(args[0], function (err, files) {
-            if (err) {
-                console.log("Le dossier '" + args[0] + "' n'existe pas.")
-            } else {
-                for (let i = 0; i < files.length; i++) {
-                    if (files[i].match('.txt')) {
-                        tab.push(files[i])
-                    }
-                }
-
-                for (let i = 1; i < args.length; i++) {
-                    cp = 0
-                    for (let j = 0; j < tab.length; j++) {
-                        if (args[i] != tab[j]) {
-                            cp++
+                    for (let i = 0; i < files.length; i++) {
+                        if (files[i].match('.txt')) {
+                            tb.push(files[i])
                         }
                     }
-                    if (cp == tab.length) {
-                        e.push(args[i])
-                    } else {
-                        t.push(args[i])
-                    }
+                    tb.forEach(file => {
+                        fs.readFile(`${args[0]}/${file}`, function (err, data) {
+                            if (err) {
+                                console.log("Le fichier '" + file + "' n'existe pas.")
+                                co++
+                            } else {
+                                if (reg.test(data)) {
+                                    console.log('Found in: ' + file)
+                                } else {
+                                    co++
+                                }
+                                if (co == tb.length) {
+                                    console.log("NONE")
+                                }
+                                
+                            }
+                        })
+                    });
                 }
-                
-                t.forEach(file => {
-                    fs.readFile(`${args[0]}/${file}`, function (err, data) {
+            })
+        })
+    }
+
+    if (arg[2].match('.txt')) {
+        console.log("Vous devez inserer un mot d'abord.")
+    } else {
+        if (arg.length <= 3) {
+            console.log("Vous avez des parametres manqants.")
+        } else {
+
+            if (arg[3].match('.txt')) {
+                args.forEach(file => {
+                    fs.readFile(file, function (err, data) {
                         if (err) {
                             console.log("Le fichier '" + file + "' n'existe pas.")
-                            co++
                         } else {
                             if (reg.test(data)) {
                                 console.log('Found in: ' + file)
                             } else {
-                                co++
+                                d++
                             }
-                            if (co == t.length) {
+                            if (d == args.length) {
                                 console.log("NONE")
                             }
                         }
-                    })
-                })
-                e.forEach(file => {
-                    console.log("Le fichier '" + file + "' n'existe pas dans le dossier '" + args[0] + "'.")
+                    });
+                });
+            } else {
+                fs.readdir(args[0], function (err, files) {
+                    if (err) {
+                        console.log("Le dossier '" + args[0] + "' n'existe pas.")
+                    } else {
+                        for (let i = 0; i < files.length; i++) {
+                            if (files[i].match('.txt')) {
+                                tab.push(files[i])
+                            }
+                        }
+
+                        for (let i = 1; i < args.length; i++) {
+                            cp = 0
+                            for (let j = 0; j < tab.length; j++) {
+                                if (args[i] != tab[j]) {
+                                    cp++
+                                }
+                            }
+                            if (cp == tab.length) {
+                                e.push(args[i])
+                            } else {
+                                t.push(args[i])
+                            }
+                        }
+
+                        t.forEach(file => {
+                            fs.readFile(`${args[0]}/${file}`, function (err, data) {
+                                if (err) {
+                                    console.log("Le fichier '" + file + "' n'existe pas.")
+                                    co++
+                                } else {
+                                    if (reg.test(data)) {
+                                        console.log('Found in: ' + file)
+                                    } else {
+                                        co++
+                                    }
+                                    if (co == t.length) {
+                                        console.log("NONE")
+                                    }
+                                }
+                            })
+                        })
+                        e.forEach(file => {
+                            console.log("Le fichier '" + file + "' n'existe pas dans le dossier '" + args[0] + "'.")
+                        })
+                    }
                 })
             }
-        })
+        }
     }
-
 }
 
 node(arg[2])
