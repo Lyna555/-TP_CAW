4e9a9a2cd3fcac8ffc7fe77e476428d622bf1764
lyna555 <bouglina3@gmail.com>
2022-12-23 21:43:14 +0100

    main

--- mini-projet/src/Components/Home.js
+++ mini-projet/src/Components/Home.js
@@ -5,6 +5,7 @@ import contact from '../Images/contact.png'
 import { Routes, Route, Link } from 'react-router-dom'
 import Contacts from './Contacts'
 import Blogs from './Blogs'
+import Main from './Main'
 import "./style.css"
 
 export default function Home() {
@@ -17,12 +18,13 @@ export default function Home() {
                         <Link id='logo' style={{color:"#ff5050",textDecoration: 'none',fontSize: 30, fontWeight: "bold"}} to='/'>MyBlogger</Link>
                     </div>
                     <div className='head2'>
-                        <Link className='link' style={{color:"black",textDecoration: 'none',fontSize: 20, fontWeight: "bold"}} to='/Contacts'><img src={contact} alt="blogger" /> Contacts</Link>
-                        <Link className='link' style={{color:"black",textDecoration: 'none',fontSize: 20, fontWeight: "bold"}} to='/Blogs'><img src={blog} alt="blogger" /> Blogs</Link>
+                        <Link className='link' style={{color:"black",textDecoration: 'none',fontSize: 23, fontWeight: "bold"}} to='/Contacts'><img src={contact} alt="blogger" /> Contacts</Link>
+                        <Link className='link' style={{color:"black",textDecoration: 'none',fontSize: 23, fontWeight: "bold"}} to='/Blogs'><img src={blog} alt="blogger" /> Blogs</Link>
                     </div>
                 </div>
             </nav>
             <Routes>
+                <Route path='/' element={<Main/>}></Route>
                 <Route path='/Contacts' element={<Contacts />}></Route>
                 <Route path='/Blogs' element={<Blogs />}></Route>
             </Routes>
---
+++ mini-projet/src/Components/Main.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Main() {
+    return (
+        <main>
+            <h1>Hello! My name is Ali Benali!</h1>
+        </main>
+    )
+}
