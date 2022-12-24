ba9ecfcc89fa9988127be7d6bef2fca379f03123
lyna555 <bouglina3@gmail.com>
2022-12-24 09:07:37 +0100

    form

--- mini-projet/src/Components/Blogs.js
+++ mini-projet/src/Components/Blogs.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Blogs() {
+  const [myBlogs, setMyBlogs] = useState([
+    {
+      id: 1,
+      subject: "Football",
+      description: "FIFA 2022 Championships held in Qatar",
+      date: "21-dec-2022"
+    },
+
+    {
+      id: 2,
+      subject: "Gym",
+      description: "I went to the gym",
+      date: "20-nov-2022"
+    },
+  ])
+
+  const [show, setShow] = useState(false)
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+  }
+
   return (
     <div className='tab'>
-      <h1>Blogs</h1>
+      <div className='row'>
+        <h1>Blogs</h1>
+        <button className='add' onClick={() => setShow(!show)}>Add Blog</button> 
+      </div>
+
+      {!show ? null : <div>
+        <form action={onSubmit}>
+          <label>Subject</label>
+          <input type="text" placeholder='Enter subject' />
+          <br />
+          <label>Description</label>
+          <input type="text" placeholder='Enter description' />
+          <br />
+          <label>Date</label>
+          <input type="date" placeholder='Enter date' />
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>}
+
       <table>
         <tbody>
           <tr>
@@ -11,11 +53,17 @@ export default function Blogs() {
             <th>Description</th>
             <th>Date</th>
           </tr>
-          <tr>
-            <td>Meeting</td>
-            <td>Meet friends during week-end</td>
-            <td>25/11/2022</td>
-          </tr>
+          {
+            myBlogs.map(blog => {
+              return (
+                <tr key={blog.id}>
+                  <td>{blog.subject}</td>
+                  <td>{blog.description}</td>
+                  <td>{blog.date}</td>
+                </tr>
+              )
+            })
+          }
         </tbody>
       </table>
     </div>
--- mini-projet/src/Components/Contacts.js
+++ mini-projet/src/Components/Contacts.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function Contacts() {
+  const [myContacts, setMyContacts] = useState([
+    {
+      id: 1,
+      name: "Douaa",
+      phone_number: "0779678196",
+      email: "douaa@gmain.com"
+    },
+
+    {
+      id: 2,
+      name: "Batoul",
+      phone_number: "0779678196",
+      email: "batoul@gmain.com"
+    },
+  ])
+
+  const [show, setShow] = useState(false)
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+  }
+
   return (
     <div className='tab'>
-      <h1>Contacts</h1>
+      <div className='row'>
+        <h1>Contacts</h1>
+        <button className='add' onClick={() => setShow(!show)}>Add Contact</button> 
+      </div>
+
+      {!show ? null : <div>
+        <form action={onSubmit}>
+          <label>Name</label>
+          <input type="text" placeholder="Enter Contact's Name" />
+          <br />
+          <label>Phone Number</label>
+          <input type="text" placeholder="Enter Contact's Phone Number" />
+          <br />
+          <label>Email</label>
+          <input type="text" placeholder="Enter Contact's Email" />
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>}
       <table>
         <tbody>
           <tr>
@@ -11,11 +53,15 @@ export default function Contacts() {
             <th>Phone Number</th>
             <th>Email</th>
           </tr>
-          <tr>
-            <td>Ali</td>
-            <td>0558061851</td>
-            <td>ali@gmail.com</td>
-          </tr>
+          {myContacts.map(contact => {
+            return (
+              <tr key={contact.id}>
+                <td>{contact.name}</td>
+                <td>{contact.phone_number}</td>
+                <td>{contact.email}</td>
+              </tr>
+            )
+          })}
         </tbody>
       </table>
     </div>
