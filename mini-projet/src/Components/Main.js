2c26473ab17aa63cea0eea2d8e6d31bec0fdec4b
lyna555 <bouglina3@gmail.com>
2022-12-29 09:26:15 +0100

    h

--- mini-projet/src/Components/Main.js
+++ mini-projet/src/Components/Main.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Main() {
     return (
         <main>
-            <h1>We are DouLy<br></br>Welcome to our homepage</h1>
+            <h1>We are DouLy<br></br>Welcome to our Homepage</h1>
         </main>
     )
 }
